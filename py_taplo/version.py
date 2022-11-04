7629759f44e90d1d63e78bffbd701203f5002c3a
Matt Krueger <mkrueger@rstms.net>
2022-11-04 07:15:46 -0500

    Bump version: 0.1.0 → 0.1.1

--- py_taplo/version.py
+++ py_taplo/version.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Matt Krueger"
 __email__ = "mkrueger@rstms.net"
-__timestamp__ = "2022-11-04 07:15:17-05:00"
+__timestamp__ = "2022-11-04 07:15:46-05:00"
